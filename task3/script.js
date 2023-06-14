d61dbd8f252f56c39737b14b07844102c41b84b8
KOLLI SRI KRISHNA KARTHIKEYA <107982190+KARTHIK174@users.noreply.github.com>
2023-06-14 18:48:53 +0530

    Update script.js

--- task3/script.js
+++ task3/script.js
@@ -1,1 +1,34 @@
+var i=1;
+function AddData() {
+    var title = document.getElementById('tit').value;
+    var description = document.getElementById('des').value;
+  
+    if (title !== "" && description !== "") {
+      var table = document.getElementById("show");
+      var row = table.insertRow();
+    
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
 
+
+      cell1.innerHTML = i;
+      cell2.innerHTML = title;
+      cell3.innerHTML = description;
+      i += 1;
+  
+      var deleteButton = document.createElement('button');
+      deleteButton.textContent = 'X';
+      deleteButton.addEventListener('click', function () {
+        var rowIndex = this.parentNode.parentNode.rowIndex;
+        table.deleteRow(rowIndex);
+      });
+  
+      cell4.appendChild(deleteButton);
+
+  
+      document.getElementById('bod').reset();
+    }
+  }
+  
