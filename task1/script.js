145d68a3a4b5f565796c4fd3d19a3ddf046a2137
KOLLI SRI KRISHNA KARTHIKEYA <107982190+KARTHIK174@users.noreply.github.com>
2023-06-14 18:45:45 +0530

    Update script.js

--- task1/script.js
+++ task1/script.js
@@ -1,1 +1,48 @@
 
+function onlyNumbers(event) {
+  var charCode = event.which ? event.which : event.keyCode;
+
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    event.preventDefault();
+    return false;
+  }
+
+  return true;
+}
+
+function brace1(){
+  document.getElementById("show").value+='(';
+}
+function brace2(){
+  document.getElementById("show").value+=')';
+}
+// Get the display element
+var display = document.getElementById("show");
+
+// Function to append a value to the display
+function appendValue(value) {
+  display.value += value;
+}
+// Function to delete the last character from the display
+display.addEventListener('click',Delete);
+function Delete() {
+  display.value = display.value.slice(0, -1);
+}
+r
+// Function to clear the display
+function Clear() {
+  display.value = "";
+}
+
+
+// Function to evaluate the expression and display the result
+function append() {
+  var expression = display.value;
+
+  try {
+    var result = eval(expression);
+    display.value = result;
+  } catch (error) {
+    display.value = "Error";
+  }
+}
